b533f7334e9c6a933f17c9aee631f220d3bb0c84
Ryo Ota <nwtgck@gmail.com>
2017-08-24 12:03:52 +0900

    [add] Use `==>`

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,12 @@ prop_always_wrong x = x+1 < x
 prop_1 x y = x+y < x
   where types = (x::Int, y::Int)
 
+prop_negative x = x * (-1) < 0
+  where types = (x :: Int)
+
+prop_negative2 x = (x <= 0) ==> (x * (-1) >= 0)
+  where types = (x :: Int)
+
 main :: IO ()
 main = do
   quickCheck (prop_RevRev :: [Int] -> Bool)
@@ -26,4 +32,7 @@ main = do
   quickCheck prop_always_wrong
   quickCheck prop_1
 
-  quickCheckWith stdArgs{maxSuccess=100000} prop_not_always_correct
+  quickCheck prop_negative
+  quickCheck prop_negative2
+
+  -- quickCheckWith stdArgs{maxSuccess=100000} prop_not_always_correct
