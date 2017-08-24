2a6803cdac8ec63ce8c270ffa4e01a177dbed5cb
Ryo Ota <nwtgck@gmail.com>
2017-08-24 11:57:56 +0900

    [add] Add not-pass checking

--- app/Main.hs
+++ app/Main.hs
@@ -10,10 +10,20 @@ prop_RevRev xs = reverse (reverse xs) == xs
 prop_not_always_correct :: Int -> Bool
 prop_not_always_correct n = n < 100
 
+prop_always_wrong x = x+1 < x
+  where types = x :: Int
+
+prop_1 x y = x+y < x
+  where types = (x::Int, y::Int)
+
 main :: IO ()
 main = do
   quickCheck (prop_RevRev :: [Int] -> Bool)
   quickCheck (prop_RevRev :: [Char] -> Bool)
   quickCheck (prop_RevRev :: [String] -> Bool)
   quickCheck prop_not_always_correct
+
+  quickCheck prop_always_wrong
+  quickCheck prop_1
+
   quickCheckWith stdArgs{maxSuccess=100000} prop_not_always_correct
